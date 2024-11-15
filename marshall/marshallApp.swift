08955c9889411d40bb035dfa29d861089784d202
Adrian Sergheev <sergheevdev@icloud.com>
2024-11-15 11:28:13 +0100

    Initial Commit

---
+++ marshall/marshallApp.swift
@@ -0,0 +1,17 @@
+//
+//  marshallApp.swift
+//  marshall
+//
+//  Created by Adrian Sergheev on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct marshallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
